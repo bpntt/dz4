975d08dccd2d40357f5f8b01e92a14830ee6ea8a
BPNTT <haataya@163.com>
2023-01-18 14:33:44 +0600

    classWork

---
+++ 002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Write Your number");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = DigCount(number);
+Console.WriteLine($"{a}");
+int DigCount(int num)
+{
+    num = Math.Abs(num);
+    int count = 0;
+    while (num >= 1)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
