fb67782e5ad221aee53e340f6aa3f6e9cf7cfbb3
BPNTT <haataya@163.com>
2023-01-18 20:45:03 +0600

    dz3

---
+++ dz3/Program.cs
@@ -0,0 +1,25 @@
+﻿int LenghtArray (string text)
+{
+    Console.WriteLine(text);
+    int result = Convert.ToInt32 ( Console.ReadLine());
+    return result;
+}
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 101);
+    }
+    return array;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+    }
+}
+int a1 = LenghtArray("Введите число");
+int[] arr = FillArray(a1);
+PrintArray(arr);
